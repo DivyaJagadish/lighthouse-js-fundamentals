0f2506cccc24b8be68ed651d061a77954f9e4959
Divya Jagadish <divyajagadish21@gmail.com>
2021-02-19 20:02:00 +0000

    pgm to find function is odd

---
+++ odd.js
@@ -0,0 +1,9 @@
+var num
+function isOdd(num)
+{
+ return ! (num%2 === 0);
+ return !(num%2 !=0);
+ }
+ console.log(isOdd(3));
+ console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
